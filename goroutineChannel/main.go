3f1a7881145033593fa7c7df453d80a6a29aef13
mojie <mojie@flamingo-inc.com>
2020-06-29 00:06:53 +0800

    add gorountine

---
+++ goroutineChannel/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	// 无缓冲通道
+	court := make(chan int)
+
+	wg.Add(2)
+
+	go player("Nadal", court)
+	go player("Djokovic", court)
+
+	// 发球
+	court <- 1
+
+	// 等待游戏结束
+	wg.Wait()
+}
+
+func player(name string, court chan int) {
+	defer wg.Done()
+
+	for {
+		ball, ok := <-court
+		if !ok {
+			// 如果通道被关闭，我们就赢了
+			fmt.Printf("Player %s Won\n", name)
+			return
+		}
+
+		n := rand.Intn(100)
+		if n%13 == 0 {
+			fmt.Printf("Player %s Missed\n", name)
+
+			// 关闭通道，表示我们输了
+			close(court)
+			return
+		}
+
+		// 显示击球数，并将击球数加1
+		fmt.Printf("Player %s Hit %d\n", name, ball)
+		ball++
+
+		// 将球打向对手
+		court <- ball
+	}
+}
