311d3e600ec35ec1eddccd457984de1a4f74e2ba
JTrancender <mkeyboard814@gmail.com>
2022-05-05 20:45:09 +0800

    feat: support route group

--- kbm-web/kbm/kbm.go
+++ kbm-web/kbm/kbm.go
@@ -1,6 +1,7 @@
 package kbm
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -9,12 +10,17 @@ type HandlerFunc func(*Context)
 
 // Engine implements the interface of ServeHttp
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup // store all groups
 }
 
 // New is the constructor of kbm.Engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 func (engine *Engine) addRouter(method, pattern string, handler HandlerFunc) {
@@ -39,4 +45,37 @@ func (engine *Engine) Run(addr string) (err error) {
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	engine.router.handle(c)
+}
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	parent      *RouterGroup  // support nesting
+	engine      *Engine       // all groups share an Engine instance
+}
+
+// Group is defined to create a new RouterGroup
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRouter(method string, pattern string, handler HandlerFunc) {
+	pattern = group.prefix + pattern
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.addRouter(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRouter("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRouter("POST", pattern, handler)
 }
--- kbm-web/main.go
+++ kbm-web/main.go
@@ -9,6 +9,34 @@ import (
 
 func main() {
 	r := kbm.New()
+	r.GET("/index", func(c *kbm.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *kbm.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello KBM </h1>")
+		})
+
+		v1.GET("/hello", func(c *kbm.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *kbm.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *kbm.Context) {
+			c.JSON(http.StatusOK, kbm.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
+
 	r.GET("/", indexHandler)
 	r.GET("/hello", helloHandler)
 	r.GET("/hello/:name", func(c *kbm.Context) {
