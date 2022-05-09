8006b935ff16456ac0865b5818486af3c5c5c0e8
JTrancender <mkeyboard814@gmail.com>
2022-05-09 23:11:04 +0800

    feat: add gopcp chanval/chanclose/chanbase/chan0cap/chantimeout/tickercase (#6)
    * feat: add gopcp chanval
    
    * feat: add gopcp chanclose
    
    * feat: add gopcp chanbase
    
    * feat: add gopcp chan0cap
    
    * feat: add gopcp chantimeout
    
    * feat: add gopcp tickercase

---
+++ gopcp/chanbase/go.mod
@@ -0,0 +1,3 @@
+module chanbase
+
+go 1.17
---
+++ gopcp/chanbase/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var strChan = make(chan string, 3)
+
+func main() {
+	syncChan1 := make(chan struct{}, 1)
+	syncChan2 := make(chan struct{}, 2)
+	go receive(strChan, syncChan1, syncChan2)
+	go send(strChan, syncChan1, syncChan2)
+	<-syncChan2
+	<-syncChan2
+}
+
+func receive(strChan <-chan string,
+	syncChan1 <-chan struct{},
+	syncChan2 chan<- struct{}) {
+	<-syncChan1
+	fmt.Println("Received a sync signal and wait a second... [receiver]")
+	time.Sleep(time.Second)
+	for elem := range strChan {
+		fmt.Printf("Received: %s. [receiver]\n", elem)
+	}
+	fmt.Println("Stopped. [receiver]")
+	syncChan2 <- struct{}{}
+}
+
+func send(strChan chan<- string,
+	syncChan1 chan<- struct{},
+	syncChan2 chan<- struct{}) {
+	for _, elem := range []string{"a", "b", "c", "d"} {
+		strChan <- elem
+		fmt.Printf("Sent: %s. [sender]\n", elem)
+		if elem == "c" {
+			syncChan1 <- struct{}{}
+			fmt.Println("Sent a sync signal. [sender]")
+		}
+	}
+	fmt.Println("Wait 2 seconds... [sender]")
+	time.Sleep(2 * time.Second)
+	close(strChan)
+	syncChan2 <- struct{}{}
+
+}
