44ecc9b2e68734eb863b710daa25b232dcc24e05
JTrancender <mkeyboard814@gmail.com>
2022-07-08 12:42:50 +0800

    feat: add hertz

---
+++ gohertz/idl/hello.thrift
@@ -0,0 +1,26 @@
+namespace go hello.example
+
+struct HelloReq {
+    1: string Name (api.query="name");
+}
+
+struct HelloResp {
+    1: string RespBody;
+}
+
+struct OtherReq {
+    1: string Other (api.body="other");
+}
+
+struct OtherResp {
+    1: string Resp;
+}
+
+service HelloService {
+    HelloResp HelloWorld(1: HelloReq request) (api.get="/hello");
+    OtherResp OtherMethod(1: OtherReq request) (api.post="/other");
+}
+
+service NewService {
+    HelloResp NewMethod(1: HelloReq request) (api.get="/new");
+}
